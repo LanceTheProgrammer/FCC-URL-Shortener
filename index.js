090956af4d428297c218f6ed98494b9433a515e6
Lance Pruitt <ljpruitt123@gmail.com>
2024-06-13 00:25:35 +0000

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,86 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const { MongoClient } = require("mongodb");
+const dns = require("dns");
+const urlparser = require("url");
+
+const client = new MongoClient(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+}
+connectDB();
+
+const db = client.db("urlshortener");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", async function (req, res) {
+  const url = req.body.url;
+
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+
+    dns.lookup(hostname, async (err, address) => {
+      if (err || !address) {
+        res.json({ error: "Invalid URL" });
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount,
+        };
+
+        const result = await urls.insertOne(urlDoc);
+        console.log(result);
+        res.json({ original_url: url, short_url: urlCount });
+      }
+    });
+  } catch (error) {
+    res.json({ error: "Invalid URL" });
+  }
 });
 
-app.listen(port, function() {
+// Redirect based on short_url
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const shorturl = req.params.short_url;
+  try {
+    const urlDoc = await urls.findOne({ short_url: +shorturl });
+    if (urlDoc) {
+      res.redirect(urlDoc.url);
+    } else {
+      res.status(404).json({ error: "Short URL not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
